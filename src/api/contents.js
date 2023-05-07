2ff36893df6661089fd376fe7e8067876031cc41
iaritina <100207807+iaritina@users.noreply.github.com>
2023-05-07 22:35:06 +0300

    Update API

--- src/api/contents.js
+++ src/api/contents.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://tp-s6-p14-web-design-mai-2022-bo-production.up.railway.app/api/contents"
+    baseURL: "https://tp-s6-p14-web-design-mai-2022-bo-production.up.railway.app/api/contents"
 });
